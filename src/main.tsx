212a8ddee5c7cf7013fbed30be511a6587281d82
Welliv <welliv@users.noreply.github.com>
2026-04-15 08:32:27 +0000

    Remove PostHog analytics, update app name to Shopstr Lightning Playground

--- src/main.tsx
+++ src/main.tsx
@@ -1,33 +1,22 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { HashRouter } from "react-router-dom";
-import { PostHogProvider } from "posthog-js/react";
 import { init } from "@getalby/bitcoin-connect-react";
 import { ThemeProvider } from "./components/theme-provider";
 import "./index.css";
 import App from "./App.tsx";
 
 init({
-  appName: "Alby Sandbox",
+  appName: "Shopstr Lightning Playground",
   showBalance: true,
 });
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <PostHogProvider
-      apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY}
-      options={{
-        api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
-        defaults: "2025-05-24",
-        capture_exceptions: true,
-        debug: import.meta.env.MODE === "development",
-      }}
-    >
-      <ThemeProvider>
-        <HashRouter>
-          <App />
-        </HashRouter>
-      </ThemeProvider>
-    </PostHogProvider>
+    <ThemeProvider>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </ThemeProvider>
   </StrictMode>,
 );
